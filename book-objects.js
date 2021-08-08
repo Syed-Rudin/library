fd1b32e36598228d3d7e79867ca1d480845d2d3c
Syed-Rudin <syedrudin04@gmail.com>
2021-08-08 16:08:56 +0800

    Added book adding feature

--- book-objects.js
+++ book-objects.js
@@ -1,6 +1,7 @@
 let bookDisplay = document.querySelector('.display');
 let newBook = document.getElementById('newBook');
-let formData = new FormData(document.querySelector('form'));
+let form = document.querySelector('form');
+let submitBtn = document.getElementById('submitBtn');
 let myLibrary = [];
 
 function Book(title, author, pages, read, notes) {
@@ -19,36 +20,63 @@ function addBookToLibrary(title, author, pages, read, notes) {
 
 
 function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
+    // for (let i = 0; i < myLibrary.length; i++) {
+    //     let book = myLibrary[i];
 
-        let card = document.createElement('div');
-        card.classList.add('card');
+    //     let card = document.createElement('div');
+    //     card.classList.add('card');
 
-        for (let key in book) {
-            if(book.hasOwnProperty(key)) {
-                if (key === 'title') {
-                    let title = document.createElement('div');  
-                    title.textContent = book[key];
-                    title.classList.add('title');
+    //     for (let key in book) {
+    //         if(book.hasOwnProperty(key)) {
+    //             if (key === 'title') {
+    //                 let title = document.createElement('div');  
+    //                 title.textContent = book[key];
+    //                 title.classList.add('title');
     
-                    card.appendChild(title);
-                } else {
-                    let feature = document.createElement('div');
-                    feature.textContent = book[key];
-                    card.appendChild(feature);
-                    console.log('got here');
-                }
+    //                 card.appendChild(title);
+    //             } else {
+    //                 let feature = document.createElement('div');
+    //                 feature.textContent = book[key];
+    //                 card.appendChild(feature);
+    //             }
+    //         }
+    //     }
+
+    //     bookDisplay.appendChild(card);
+    // }
+    // myLibrary = [];
+
+    let lastBook = myLibrary[myLibrary.length - 1];
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    for (let key in lastBook) {
+        if(lastBook.hasOwnProperty(key)) {
+            if (key === 'title') {
+                let title = document.createElement('div');  
+                title.textContent = lastBook[key];
+                title.classList.add('title');
+
+                card.appendChild(title);
+            } else {
+                let feature = document.createElement('div');
+                feature.textContent = lastBook[key];
+                card.appendChild(feature);
             }
         }
-
-        bookDisplay.appendChild(card);
     }
+
+    bookDisplay.appendChild(card);
 }
 
-addBookToLibrary('HP', 'JKR', 235, 'read', 'nice book');
-addBookToLibrary('ATTWN', 'AC', 277, 'read', 'great book');
-addBookToLibrary('HP', 'JKR', 235, 'read', 'nice book');
-addBookToLibrary('ATTWN', 'AC', 277, 'read', 'great book');
+submitBtn.addEventListener('click', function(e) {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
+    let notes = document.getElementById('notes').value;
+    addBookToLibrary(title, author, pages, read, notes);
+    displayBooks();
 
-displayBooks();
+    form.reset();
+})
